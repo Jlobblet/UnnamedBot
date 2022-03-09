13bcc1896ae9c47a9a73ea2faf7f3d2b1d332394
Jlobblet <jlobblet-github@gmx.co.uk>
2022-03-09 00:47:18 +0000

    Format a bit

--- src/main.rs
+++ src/main.rs
@@ -8,17 +8,25 @@ mod config;
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let level = if cfg!(debug_assertions) { Level::Debug } else { Level::Info };
+    let level = if cfg!(debug_assertions) {
+        Level::Debug
+    } else {
+        Level::Info
+    };
     simple_logger::init_with_level(level)?;
     info!("Starting bot");
+
     debug!("Creating config");
     let cfg = Config::try_default()?;
+
     debug!("Creating framework");
     let framework = bot::default_framework(&cfg);
+
     debug!("Creating client");
     let mut client = bot::default_client(&cfg, framework)
         .await
         .context("Failed to get client")?;
+
     info!("Connecting to Discord");
     client.start().await.context("Failed to start client")?;
 
